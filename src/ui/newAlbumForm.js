84c518b2c9feb5b0f9b7cc8282578702d8aee90e
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-11-03 00:09:06 -0700

    styled cancel button and added functionality to remove recently created album

--- src/ui/newAlbumForm.js
+++ src/ui/newAlbumForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { hashHistory, Link } from 'react-router'
-import { updateAlbum } from 'apis/api'
+import { updateAlbum, deleteAlbum } from 'apis/api'
 
 
 
@@ -22,7 +22,13 @@ const AlbumForm = React.createClass({
 		this.setState(obj);
 	},
 
-	
+	goBack: function(e) {
+		e.preventDefault()
+		deleteAlbum(this.props.params.id)
+		hashHistory.goBack()
+
+	},	
+
 	updateAlbum: function() { 
 		var obj = {
 			album_label: this.state.album_label,
@@ -41,10 +47,10 @@ const AlbumForm = React.createClass({
 
 	render: function() {
 		return (
-			<div id="albumHeader">Jamie's Gothic Photo Album
+			<div id="albumHeader" className="relative">Jamie's Gothic Photo Album
 				<input className="formInput" id="album_label" onChange={this.update} type="text" placeholder="Album Name"></input>
 				<input className="formInput" id="album_cover_pic" onChange={this.update} type="text" placeholder="Album Cover Pic URL"></input>
-				<button onClick={this.postToAPI}>Submit</button>
+				<button onClick={this.postToAPI} className="addButton albumForm">Submit</button>
 				<button id="goBack" onClick={this.goBack}>Cancel</button>
 			</div>
 		)
